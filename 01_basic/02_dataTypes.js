8461ac7aba651e8eb98da5534b9308ab202ab16a
Pareshkumar <krparesh03@gmail.com>
2024-09-10 12:26:19 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//use all js code as newer version
+
+//alert (3 + 3) we are using nodejs not browser
+//code readability should be high
+
+console.log(3 + 3)
+console.log("paresh")
+
+let name = "Paresh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// * Basic dataTypes
+
+//number => 2 to power 53
+//bigint
+//string""
+//boolean=>trur/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+// * object
+
+console.log(typeof undefined)//undefine
+console.log(typeof null)//object
