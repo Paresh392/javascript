12a8569b53f0eb6dc5aa0278b1b0cb2b20867c82
Pareshkumar <krparesh03@gmail.com>
2024-09-11 11:52:26 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -33,4 +33,32 @@ let stringNumber = String(someNumber)
 
  let value = 3
  let negValue = -value
- console.log(negValue)
+ //console.log(negValue)
+
+ 
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+let str1= "hello"
+let str2= " Paresh "
+
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(2 + 2 + "3");
+
+console.log((3 + 4) * 3 % 5 );
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
