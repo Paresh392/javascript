55e6760cf51f26e0a5db3819b4652f7277223338
Pareshkumar <krparesh03@gmail.com>
2024-09-12 18:28:45 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,8 @@
+//concatenaton of string
+
+const name = "Paresh"
+const repocont = 50
+
+//conosole.log(name + repocount + "value"); //not better ways to concatenate and not relilable.
+
+console.log(`helllo my name is ${name} and my repo count is ${repocount}`);
