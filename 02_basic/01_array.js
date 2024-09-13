acd27d45ae44acfbca2cf86e2159be5b236bc686
Pareshkumar <krparesh03@gmail.com>
2024-09-13 13:44:23 +0000

    Array in js

---
+++ 02_basic/01_array.js
@@ -0,0 +1,30 @@
+// Array
+
+const myArray =  [0, 1, 2, 3, 4, 5, 6] //Array of js is resizable
+const myHeros = ["thor", "iron man", "CA"]
+
+// console.log(myArray[3]);
+// console.log(myHeros[0]);
+
+const myArray2 = new Array(1, 2, 3, 4)
+// console.log(myArray[1]);
+
+
+  //  Array Methods:
+
+//   myArray.push(6)
+//   myArray.push(7)
+//   myArray.pop(5)
+//   myArray.pop(6) 
+
+// myArray.unshift(9);
+// myArray.shift();
+
+// console.log(myArray.includes(7));
+// console.log(myArray.indexOf(3));
+
+const newArray = myArray.join()
+
+console.log(myArray);
+console.log(newArray);
+console.log(typeof newArray);
