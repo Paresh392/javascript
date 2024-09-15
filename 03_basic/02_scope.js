5023faec191c957bee49213e7dd6b2ab26970f84
Pareshkumar <krparesh03@gmail.com>
2024-09-15 19:28:07 +0000

    Hoisting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -3,11 +3,47 @@ let a = 200
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER:",a);
+    //console.log("INNER:",a);
     
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
+//console.log(c);
 
+function one(){
+    const username = "paresh"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    //two()
+}
+//one()
+
+if (true) {
+    const username = "paresh"
+    if (username === "paresh") {
+        const website = " youtube"
+        //console.log(username +  website);
+        }
+        //console.log(website);
+        
+    }
+    //console.log(username);
+
+    //*****************. interesting *************
+  
+ console.log(addone(5))
+   function addone(num){
+    return num + 1
+ } 
+ //addtwo(5)
+ const addtwo = function(num){    //  Hoisting
+      return num + 2
+ }
+ addtwo(5)
