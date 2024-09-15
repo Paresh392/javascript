f2d2068b77290481334379ebd067f6665c684af8
Pareshkumar <krparesh03@gmail.com>
2024-09-15 18:45:58 +0000

    object

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -41,5 +41,38 @@ const result = addTwoNumbers(3 ,5)
  }
 
  //console.log(loginUserMessage("paresh"))
- console.log(loginUserMessage("paresh"))
+ //console.log(loginUserMessage("paresh"))
+
+ // shopping cart. 
+
+function calculateCartPrice (val1, val2,...num1) {
+    return num1
+}
+//console.log(calculateCartPrice( 200 , 300 , 400));
+
+//. passing object in function. 
+
+const user = {
+    Username: "paresh",
+    price: 200
+}
+ function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
  
+// handleObject(user)
+handleObject({
+     username: "sam",
+     price: 200
+})
+
+      //. passing array
+
+  const MyNewArray = [200, 400, 500, 600]
+  
+  function returnSecondValue(getArray) {
+    return getArray[1]
+  }
+
+//   console.log(returnSecondValue(MyNewArray));
+console.log(returnSecondValue([200, 300, 500]));
