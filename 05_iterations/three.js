0d761b2d774fb20b30142f902ad82555d5177539
Pareshkumar <krparesh03@gmail.com>
2024-10-10 18:46:58 +0000

    maps

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,14 +3,39 @@
 // ["", "", ""]
 // [ {}, {}, {}]
 
-const arr = [1, 2, 3, 4, 5]
+// const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
-   // console.log(num);
-}
+// for (const num of arr) {
+//    // console.log(num);
+// }
 
-const greeting = "Hello world"
-for (const greet of greeting) {
-    console.log(`each char is ${greet}`)
+// const greeting = "Hello world"
+// for (const greet of greeting) {
+//     console.log(`each char is ${greet}`)
+    
+// }
+
+// Maps
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United Status of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, `:-` , value );
     
 }
+
+const myObject = {
+    'game 1' : 'NFC', 
+    'game 2' : 'spiderman' 
+}
+// for (const [key, value] of map) {
+//     console.log(key, `:-` , value );
+                                          
+// }.  //  not work
